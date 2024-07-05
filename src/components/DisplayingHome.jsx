8dcbf13a671016adf93a8c4e6f2d06a1fea7e8b8
Shubham <shubhambhardwaj112277@gmail.com>
2024-07-05 13:15:01 +0530

    Added Albumitem and Songitem on Homepage

--- src/components/DisplayingHome.jsx
+++ src/components/DisplayingHome.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData, songsData } from '../assets/frontend-assets/assets'
+import Albumitem from './Albumitem'
+import Songitem from './Songitem'
 
 const DisplayingHome = () => {
   return (
     <>
     <Navbar/>
+    <div className=" mb-4">
+      <h1 className=' my-5 font-bold text-2xl'>Featured Charts</h1>
+      <div className="flex overflow-auto">
+      {
+        albumsData.map((item,index)=>(<Albumitem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id}/>))
+      }
+      </div>
+     
+    </div>
+    <div className=" mb-4">
+      <h1 className=' my-5 font-bold text-2xl'>Today's biggest hits</h1>
+      <div className="flex overflow-auto">
+      {
+        songsData.map((item,index)=>(<Songitem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>))
+      }
+      </div>
+     
+    </div>
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import {assets} from '../assets/frontend-assets/assets.js'
 
 const Sidebar = () => {
   return (
-    <div className=' w-[35%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
+    <div className=' w-[30%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         <div className=" bg-[#121212] h-[15%] rounded flex flex-col justify-around">
             <div className=" flex items-center gap-3 pl-8 cursor-pointer">
                 <img className=' w-6' src={assets.home_icon} alt="" />
