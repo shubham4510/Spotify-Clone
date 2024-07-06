ad43a361e8d7f7b21b4e2cebd20fadf646803269
Shubham <shubhambhardwaj112277@gmail.com>
2024-07-06 13:47:16 +0530

    Added Music play and pause functionality

--- src/components/Albumitem.jsx
+++ src/components/Albumitem.jsx
@@ -4,11 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const Albumitem = ({image,name,desc,id}) => {
 
   const navigate = useNavigate();
-
-
-
-
-
+  
   return (
     <div onClick={()=>navigate(`/album/${id}`)} className=' min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className=' rounded' src={image} alt="" />
